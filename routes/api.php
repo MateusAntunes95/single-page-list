34b48db31f9557924999bc3df4a489708b601719
Mateus Antunes <mateus.antunes95@gmail.com>
2023-08-30 12:24:04 -0300

    Implementa JWT

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function(Request $request) {
+    $credentials = $request->only(['email', 'password']);
+
+    if (! $token = auth()->attempt($credentials)) {
+        abort(401, 'Não autorizado');
+    }
+
+    return response()->json([
+        'data' => [
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 120
+        ]
+    ]);
 });
 
 Route::controller(TaskController::class)->group(function () {
